83041d6ec9f3a3dea910857230723c3f18e582b7
Thomas Gamble <thomasgamble@gokaconsulting.com>
2013-03-02 14:32:03 -0500

    Added twitter bootstrap files

--- app.js
+++ app.js
@@ -11,27 +11,57 @@ var express = require('express')
 
 var app = express();
 
-app.configure(function(){
+app.configure(function()
+{
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.set('title', 'Benefleet');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser('your secret here'));
   app.use(express.session());
+  app.use(function(req, res, next){
+        var err = req.session.error
+            , msg = req.session.success;
+        delete req.session.error;
+        delete req.session.success;
+        res.locals.message = '';
+        if (err) res.locals.message = '<p class="msg error">' + err + '</p>';
+        if (msg) res.locals.message = '<p class="msg success">' + msg + '</p>';
+        next();
+  });
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
+
+function restrict(req, res, next) {
+    if (req.session.user) {
+        console.log("Access by logged in user");
+        next();
+    } else {
+        req.session.error = 'Access denied!';
+        res.redirect('/login');
+    }
+}
+
+app.get('/', restrict, routes.index);
+app.get('/users', restrict, user.list);
+app.get('/login', function(req, res){
+    res.render('login');
+});
+app.post('/login', user.login);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+  console.log("Express server listening on port %d in %s mode", app.get('port'), app.get('env'));
 });
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,7 @@
 
+var hash = require('pwd').hash;
+
+
 /*
  * GET users listing.
  */
@@ -6,3 +9,71 @@
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
+
+exports.logout = function (req, res) {
+    // destroy the user's session to log them out
+    // will be re-created next request
+    req.session.destroy(function () {
+        res.redirect('/');
+    })
+};
+
+exports.login = function(req, res){
+    authenticate(req.body.username, req.body.password, function(err, user){
+        if (user) {
+            // Regenerate session when signing in
+            // to prevent fixation
+            req.session.regenerate(function(){
+                // Store the user's primary key
+                // in the session store to be retrieved,
+                // or in this case the entire user object
+                req.session.user = user;
+                req.session.success = 'Authenticated as ' + user.name
+                    + ' click to <a href="/logout">logout</a>. '
+                    + ' You may now access <a href="/restricted">/restricted</a>.';
+                res.redirect('/');
+            });
+        } else {
+            console.log("Authentication failed");
+            req.session.error = 'Authentication failed, please check your '
+                + ' username and password.'
+                + ' (use "tj" and "foobar")';
+            res.redirect('login');
+        }
+    });
+};
+
+// dummy database
+
+var users = {
+    tj: { name: 'tj' }
+};
+
+// when you create a user, generate a salt
+// and hash the password ('foobar' is the pass here)
+hash('foobar', function(err, salt, hash){
+    if (err) throw err;
+    // store the salt & hash in the "db"
+    users.tj.salt = salt;
+    users.tj.hash = hash;
+});
+
+
+// Authenticate using our plain-object database of doom!
+
+function authenticate(name, pass, fn) {
+    if (!module.parent) console.log('authenticating %s:%s', name, pass);
+    var user = users[name];
+    // query the db for the given username
+    if (!user) return fn(new Error('cannot find user'));
+    // apply the same algorithm to the POSTed password, applying
+    // the hash against the pass / salt, if there is a match we
+    // found the user
+    hash(pass, user.salt, function (err, hash) {
+        console.log("in hash");
+        if (err) return fn(err);
+        if (hash == user.hash) return fn(null, user);
+        fn(new Error('invalid password'));
+    });
+}
+
